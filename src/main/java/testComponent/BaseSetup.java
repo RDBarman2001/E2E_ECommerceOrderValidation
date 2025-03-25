5959a1b344ecd1b7a2d5d2f73b0b3486bf9d4bcf
Rupak <rb00993323@techmahindra.com>
2025-03-25 18:06:01 +0530

    chrome headless update

--- src/main/java/testComponent/BaseSetup.java
+++ src/main/java/testComponent/BaseSetup.java
@@ -11,10 +11,12 @@ import java.util.List;
 import java.util.Properties;
 
 import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
@@ -28,16 +30,22 @@ import base.LoginPage;
 public class BaseSetup {
 	public WebDriver driver;
 	public LoginPage loginPage;
+
 	public WebDriver setupDriver() throws IOException {
 		Properties prop = new Properties();
-		FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"//src//main//java//GlobalData.properties");
+		FileInputStream fis = new FileInputStream(
+				System.getProperty("user.dir") + "//src//main//java//GlobalData.properties");
 		prop.load(fis);
 		String browserName = prop.getProperty("browser");
 
-		if (browserName.equalsIgnoreCase("Chrome")) {
-
-			driver = new ChromeDriver();
-
+		if (browserName.contains("chrome")) {
+			
+			ChromeOptions options = new ChromeOptions();
+			if(browserName.contains("headless")) {
+			options.addArguments("headless");
+			}
+			driver = new ChromeDriver(options);
+			driver.manage().window().setSize(new Dimension(1440,900));//full screen in headless
 		}
 
 		else if (browserName.equalsIgnoreCase("Firefox")) {
@@ -53,46 +61,43 @@ public class BaseSetup {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		return driver;
 	}
-	
-	@BeforeMethod(alwaysRun=true)
+
+	@BeforeMethod(alwaysRun = true)
 
 	public LoginPage launchApp() throws IOException {
-		
-		driver= setupDriver();
+
+		driver = setupDriver();
 		loginPage = new LoginPage(driver);
 		loginPage.goTo();
 		return loginPage;
-		
+
 	}
-	
-	
-	
-	@AfterMethod(alwaysRun=true)
+
+	@AfterMethod(alwaysRun = true)
 	public void tearDown() {
 		driver.close();
 	}
-	
-	
+
 	public List<HashMap<String, String>> getjsoDataToMap(String filePath) throws IOException {
-		//read json to string 
-	String jsonContent =	FileUtils.readFileToString(new File(filePath),StandardCharsets.UTF_8);
-		
-	//String to HashMap : jackson databind
-	ObjectMapper mapper = new ObjectMapper();
-	List<HashMap<String,String>> data = mapper.readValue(jsonContent, new TypeReference<List<HashMap<String,String>>>() {});
-	
-	return data;
+		// read json to string
+		String jsonContent = FileUtils.readFileToString(new File(filePath), StandardCharsets.UTF_8);
+
+		// String to HashMap : jackson databind
+		ObjectMapper mapper = new ObjectMapper();
+		List<HashMap<String, String>> data = mapper.readValue(jsonContent,
+				new TypeReference<List<HashMap<String, String>>>() {
+				});
+
+		return data;
 	}
-	
-	
-public String getSS(String testName,WebDriver driver) throws IOException {
 
-	TakesScreenshot ts = (TakesScreenshot)driver;
-	File source = ts.getScreenshotAs(OutputType.FILE);
-	File file = new File(System.getProperty("user.dir")+"//reports//"+ testName + ".png");
-	FileUtils.copyFile(source, file);
-	return System.getProperty("user.dir")+"//reports//"+ testName + ".png";
-}
+	public String getSS(String testName, WebDriver driver) throws IOException {
 
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File source = ts.getScreenshotAs(OutputType.FILE);
+		File file = new File(System.getProperty("user.dir") + "//reports//" + testName + ".png");
+		FileUtils.copyFile(source, file);
+		return System.getProperty("user.dir") + "//reports//" + testName + ".png";
+	}
 
 }
